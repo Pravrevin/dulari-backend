0e80920723ff58b38bbccc3e6f36738762e97ece
pravrevin <praveenkrrc@gmail.com>
2026-05-02 17:34:55 +0530

    Image changes

--- dulari_backend/urls.py
+++ dulari_backend/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.http import JsonResponse
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 from .reset_view import reset_and_load
 
@@ -18,4 +18,9 @@ urlpatterns = [
     path("api/", include("products.urls")),
     path("api/auth/", include("users.urls")),
     path("api/contact/", include("contact.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
